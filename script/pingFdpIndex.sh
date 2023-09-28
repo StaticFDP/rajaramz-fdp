534a47c0119dad7412558c73eb8801e3d6a0710d
Eric Prud'hommeaux <eric+github@w3.org>
2023-09-28 10:28:46 +0200

    + shebang

--- script/pingFdpIndex.sh
+++ script/pingFdpIndex.sh
@@ -1,1 +1,3 @@
+#!/bin/bash
+
 curl -X POST -H 'Content-Type: application/json' -d "{\"clientUrl\": \"${FDP_URL}\"}" ${FDP_INDEX_URL} --fail-with-body && echo "asked <${FDP_INDEX_URL}> to index <${FDP_URL}>"
